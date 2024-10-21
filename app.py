a06e6f639915a8806a6eb935aa9e6964d5a7efe0
kmueller <chaspar@web.de>
2024-10-21 15:26:17 -0700

    feat: Integrate chainlit front end

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import uuid
 import time
 
+import chainlit as cl
+
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
@@ -35,6 +37,7 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, StateGraph, START
 from langgraph.prebuilt import tools_condition
 
+_printed = set()
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
@@ -63,120 +66,170 @@ class Assistant:
                 break
         return {"messages": result}
 
+@cl.on_chat_start
+async def on_chat_start():
+    print("Chat started")
+    # Haiku is faster and cheaper, but less accurate
+    # llm = ChatAnthropic(model="claude-3-haiku-20240307")
+    # llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=1)
+    API_HOST = os.getenv("API_HOST")
+    if API_HOST == "github":
+        llm = ChatOpenAI(
+        openai_api_base="https://models.inference.ai.azure.com",
+        api_key=os.getenv("GITHUB_TOKEN"),
+        model=os.getenv("GITHUB_MODEL")
+    )
+    else:
+        raise ValueError("Unsupported API host")
 
-# Haiku is faster and cheaper, but less accurate
-# llm = ChatAnthropic(model="claude-3-haiku-20240307")
-# llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=1)
-API_HOST = os.getenv("API_HOST")
-if API_HOST == "github":
-    llm = ChatOpenAI(
-    openai_api_base="https://models.inference.ai.azure.com",
-    api_key=os.getenv("GITHUB_TOKEN"),
-    model=os.getenv("GITHUB_MODEL")
-)
-else:
-    raise ValueError("Unsupported API host")
+    # You could swap LLMs, though you will likely want to update the prompts when
+    # doing so!
+    # from langchain_openai import ChatOpenAI
 
-# You could swap LLMs, though you will likely want to update the prompts when
-# doing so!
-# from langchain_openai import ChatOpenAI
+    # llm = ChatOpenAI(model="gpt-4-turbo-preview")
 
-# llm = ChatOpenAI(model="gpt-4-turbo-preview")
+    primary_assistant_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful customer support assistant for Swiss Airlines. "
+                "Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+                "When searching, be persistent. Expand your query bounds if the first search returns no results. "
+                "If a search comes up empty, expand your search before giving up."
+                "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+                "\nCurrent time: {time}.",
+            ),
+            ("placeholder", "{messages}"),
+        ]
+    ).partial(time=datetime.now())
 
-primary_assistant_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful customer support assistant for Swiss Airlines. "
-            "Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
-            "When searching, be persistent. Expand your query bounds if the first search returns no results. "
-            "If a search comes up empty, expand your search before giving up."
-            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
-            "\nCurrent time: {time}.",
-        ),
-        ("placeholder", "{messages}"),
+    # lookup_policy,
+    part_1_tools = [
+        TavilySearchResults(max_results=1),
+        fetch_user_flight_information,
+        search_flights,
+        update_ticket_to_new_flight,
+        cancel_ticket,
+        search_car_rentals,
+        book_car_rental,
+        update_car_rental,
+        cancel_car_rental,
+        search_hotels,
+        book_hotel,
+        update_hotel,
+        cancel_hotel,
+        search_trip_recommendations,
+        book_excursion,
+        update_excursion,
+        cancel_excursion,
     ]
-).partial(time=datetime.now())
+    part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
 
-# lookup_policy,
-part_1_tools = [
-    TavilySearchResults(max_results=1),
-    fetch_user_flight_information,
-    search_flights,
-    update_ticket_to_new_flight,
-    cancel_ticket,
-    search_car_rentals,
-    book_car_rental,
-    update_car_rental,
-    cancel_car_rental,
-    search_hotels,
-    book_hotel,
-    update_hotel,
-    cancel_hotel,
-    search_trip_recommendations,
-    book_excursion,
-    update_excursion,
-    cancel_excursion,
-]
-part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
+    builder = StateGraph(State)
 
-builder = StateGraph(State)
+    # Define nodes: these do the work
+    builder.add_node("assistant", Assistant(part_1_assistant_runnable))
+    builder.add_node("tools", create_tool_node_with_fallback(part_1_tools))
+    # Define edges: these determine how the control flow moves
+    builder.add_edge(START, "assistant")
+    builder.add_conditional_edges(
+        "assistant",
+        tools_condition,
+    )
+    builder.add_edge("tools", "assistant")
 
-# Define nodes: these do the work
-builder.add_node("assistant", Assistant(part_1_assistant_runnable))
-builder.add_node("tools", create_tool_node_with_fallback(part_1_tools))
-# Define edges: these determine how the control flow moves
-builder.add_edge(START, "assistant")
-builder.add_conditional_edges(
-    "assistant",
-    tools_condition,
-)
-builder.add_edge("tools", "assistant")
+    # The checkpointer lets the graph persist its state
+    # this is a complete memory for the entire graph.
+    memory = MemorySaver()
+    part_1_graph = builder.compile(checkpointer=memory)
 
-# The checkpointer lets the graph persist its state
-# this is a complete memory for the entire graph.
-memory = MemorySaver()
-part_1_graph = builder.compile(checkpointer=memory)
+    # save graph and state to the user session
+    cl.user_session.set("graph", part_1_graph)
 
-# Let's create an example conversation a user might have with the assistant
-tutorial_questions = [
-    "Hi there, what time is my flight?",
-    "Am i allowed to update my flight to something sooner? I want to leave later today.",
-    "Update my flight to sometime next week then",
-    "The next available option is great",
-    "what about lodging and transportation?",
-    "Yeah i think i'd like an affordable hotel for my week-long stay (7 days). And I'll want to rent a car.",
-    "OK could you place a reservation for your recommended hotel? It sounds nice.",
-    "yes go ahead and book anything that's moderate expense and has availability.",
-    "Now for a car, what are my options?",
-    "Awesome let's just get the cheapest option. Go ahead and book for 7 days",
-    "Cool so now what recommendations do you have on excursions?",
-    "Are they available while I'm there?",
-    "interesting - i like the museums, what options are there?",
-    "OK great pick one and book it for my second day there.",
-]
 
-# Update with the backup file so we can restart from the original place in each section
-db.update_dates()
-thread_id = str(uuid.uuid4())
+@cl.on_message
+async def on_chat_message(message: cl.Message):
+    part_1_graph: Runnable = cl.user_session.get("graph")
 
-config = {
-    "configurable": {
-        # The passenger_id is used in our flight tools to
-        # fetch the user's flight information
-        "passenger_id": "3442 587242",
-        # Checkpoints are accessed by thread_id
-        "thread_id": thread_id,
+   
+
+    # Update with the backup file so we can restart from the original place in each section
+    db.update_dates()
+    thread_id = str(uuid.uuid4())
+
+    config = {
+        "configurable": {
+            # The passenger_id is used in our flight tools to
+            # fetch the user's flight information
+            "passenger_id": "3442 587242",
+            # Checkpoints are accessed by thread_id
+            "thread_id": thread_id,
+        }
     }
-}
 
-_printed = set()
-for question in tutorial_questions:
+    ui_message = cl.Message(content="")
+    await ui_message.send()
     events = part_1_graph.stream(
-        {"messages": ("user", question)}, config, stream_mode="values"
+        {"messages": ("user", message.content)}, config, stream_mode="values"
     )
     for event in events:
-        _print_event(event, _printed)
-        
+        #_print_event(event, _printed)
+        # print(f"event: {event}")
+        #print(type(event))
+        current_state = event.get("dialog_state")
+        #print(f"current_state: {current_state}")
+        message = event.get("messages")
+        print(f"message: {message}")
+        print(f"message -1 content: {message[-1].content}")
+        print(f"message type: {message[-1].type}")
+        if message[-1].type == "ai":
+            await ui_message.stream_token(token=message[-1].content)
+    await ui_message.update()
+
     # Sleep for a bit to allow the API to work with rate limits
-    time.sleep(10)
+   # time.sleep(10)
+
+
+def run_with_preset_conversation():
+     # Let's create an example conversation a user might have with the assistant
+    tutorial_questions = [
+        "Hi there, what time is my flight?",
+        "Am i allowed to update my flight to something sooner? I want to leave later today.",
+        "Update my flight to sometime next week then",
+        "The next available option is great",
+        "what about lodging and transportation?",
+        "Yeah i think i'd like an affordable hotel for my week-long stay (7 days). And I'll want to rent a car.",
+        "OK could you place a reservation for your recommended hotel? It sounds nice.",
+        "yes go ahead and book anything that's moderate expense and has availability.",
+        "Now for a car, what are my options?",
+        "Awesome let's just get the cheapest option. Go ahead and book for 7 days",
+        "Cool so now what recommendations do you have on excursions?",
+        "Are they available while I'm there?",
+        "interesting - i like the museums, what options are there?",
+        "OK great pick one and book it for my second day there.",
+    ]
+
+    # Update with the backup file so we can restart from the original place in each section
+    db.update_dates()
+    thread_id = str(uuid.uuid4())
+
+    config = {
+        "configurable": {
+            # The passenger_id is used in our flight tools to
+            # fetch the user's flight information
+            "passenger_id": "3442 587242",
+            # Checkpoints are accessed by thread_id
+            "thread_id": thread_id,
+        }
+    }
+
+    _printed = set()
+    for question in tutorial_questions:
+        events = part_1_graph.stream(
+            {"messages": ("user", question)}, config, stream_mode="values"
+        )
+        for event in events:
+            _print_event(event, _printed)
+
+        # Sleep for a bit to allow the API to work with rate limits
+        time.sleep(10)
